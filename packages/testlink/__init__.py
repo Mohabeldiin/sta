85ce2b7bdee64bad4207318caff4db7fb3313296
Mohab Mohsen <mohabeldiin@gmail.com>
2022-06-14 23:11:19 +0200

    moveing package into init

--- packages/testlink/__init__.py
+++ packages/testlink/__init__.py
@@ -1,5 +1,17 @@
-"""Gets the Link from the database"""
-from .testlink import get_link
+"""Gets the Link from the database
+
+    this file is used as base method for test scripts
+    :returns: Link"""
+import json
+import requests
+
+
+def get_link():
+    """Gets the Link from the database"""
+    link = "https://a5r-testing.herokuapp.com/getLink"
+    response = requests.get(link)
+    data = json.loads(response.text)
+    return data['get'][-1]['link']
 
 __author__ = "Mohab Mohsen"
 __license__ = "MIT"
