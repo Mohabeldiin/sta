4a2634c9d221f42b42efe013418938f9a4a05310
Mohab Mohsen <mohabeldiin@gmail.com>
2022-06-14 23:40:22 +0200

    adding ogger and rename method

--- packages/testlink/__init__.py
+++ packages/testlink/__init__.py
@@ -3,15 +3,30 @@
     this file is used as base method for test scripts
     :returns: Link"""
 import json
-import requests
+
+from logger import project_logger
+
+logger = project_logger("Test Link")
+
+try:
+    import requests
+except ImportError:
+    logger.error("Requests is not installed")
+    raise ImportError("Please install requests module") from ImportError
 
 
-def get_link():
+def get_link_to_test():
     """Gets the Link from the database"""
+    logger.info("Getting Link from Database")
     link = "https://a5r-testing.herokuapp.com/getLink"
+    logger.debug("Requesting Link")
     response = requests.get(link)
+    logger.debug("Response Received")
     data = json.loads(response.text)
+    logger.debug("Link Received: %s", data['get'][-1]['link'])
+    logger.info("Link Received")
     return data['get'][-1]['link']
+
 
 __author__ = "Mohab Mohsen"
 __license__ = "MIT"
