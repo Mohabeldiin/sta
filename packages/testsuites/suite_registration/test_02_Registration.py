bab365590d3809a876a2ffe92f2ee3b9431aaa68
Mohab Mohsen <mohabeldiin@gmail.com>
2022-06-18 18:34:18 +0200

    edit

--- packages/testsuites/suite_registration/test_02_Registration.py
+++ packages/testsuites/suite_registration/test_02_Registration.py
@@ -4,7 +4,7 @@
 
 from packages.testsuites.suite_registration.init import *  # pylint: disable = unused-wildcard-import, wildcard-import
 
-logger = project_logger("Registration Test Case 1")
+logger = project_logger("Registration Test Case 2")
 
 
 class Test_02_Registration(unittest.TestCase):  # pylint: disable = invalid-name
@@ -12,25 +12,21 @@ class Test_02_Registration(unittest.TestCase):  # pylint: disable = invalid-name
         2-Click on the Register button."""
 
     def setUp(self):
-        """this method will be called before every test"""
+        """called before every test"""
         self.driver = setup_selenium_driver()
         SetUp(self, self.driver)
 
     def test_01_Required_Fields(self):  # pylint: disable = invalid-name
         """Check the required fields by not filling any data"""
         self.sinup.click()  # pylint: disable = no-member
-        a1 = self.email.get_attribute(  # pylint: disable = no-member
-            "value aria-required")  # pylint: disable = no-member
-        a2 = self.password.get_attribute(  # pylint: disable = no-member
-            "aria-required") == "true"  # pylint: disable = no-member
         self.assertTrue(self.email.get_attribute("aria-required"),  # pylint: disable = no-member
                         "Fields are not Required")
         self.assertTrue(self.password.get_attribute("aria-required") == "true",  # pylint: disable = no-member
                         "Fields are not Required")
 
     def tearDown(self):
-        """this method will be called after every test"""
-        self.driver.quit()
+        """called after every test"""
+        TearDown(self.driver)
 
 
 if __name__ == "__main__":
