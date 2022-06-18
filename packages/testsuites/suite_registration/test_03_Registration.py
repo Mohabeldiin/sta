0ca1c6e4877c67f73c0d07e340437c4bbd4d6c5b
Mohab Mohsen <mohabeldiin@gmail.com>
2022-06-18 19:50:56 +0200

    edit
    to be contuined

--- packages/testsuites/suite_registration/test_03_Registration.py
+++ packages/testsuites/suite_registration/test_03_Registration.py
@@ -1,33 +1,49 @@
-"""this test case Check user should Register by filling all the required fields
-    TC_03_Registration from https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
-import unittest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+"""Check user should Register by filling all the required fields
+    TC_03_Registration
+    Refer to https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
+
+from packages.testsuites.suite_registration.init import (
+    SetUp, TearDown, project_logger, TestData, unittest, setup_selenium_driver)
+from packages.tempmail import TempMail
+
+logger = project_logger("Registration Test Case 2")
 
 
-class Test_03_Registration(unittest.TestCase):
+class Test_03_Registration(unittest.TestCase):  # pylint: disable = invalid-name
     """1- Enter valid values in the required fields.
        2- Click on the Register button."""
-    
+
     def setUp(self):
         """this method will be called before every test"""
-        self.driver = webdriver.Chrome("C:\\Program Files (x86)\\chromedriver.exe")
-        self.driver.implicitly_wait(5)
-        self.driver.get('https://www.facebook.com/')
-        self.driver.maximize_window()
+        self.driver = setup_selenium_driver()
+        SetUp(self, self.driver)
+        self.temp_mail = TempMail()
+        self.test_data = TestData()
 
-
-    def test_01_Required_fields(self):
+    def test_01_required_fields(self):
         """Check user should Register by filling all the required fields"""
-        pass
-
+        self.firstname.send_keys(  # pylint:disable=no-member
+            TestData.FRIST_NAME)
+        self.lasttname.send_keys(  # pylint:disable=no-member
+            TestData.LAST_NAME)
+        email = self.temp_mail.get_email()
+        self.email.send_keys(email)  # pylint:disable=no-member
+        self.reemail.send_keys(email)  # pylint:disable=no-member
+        self.password.send_keys(  # pylint:disable=no-member
+            TestData.PASSWORD_NUM_LETTER)
+        self.birthday.click()  # pylint:disable=no-member
+        self.birthmonth.click()  # pylint:disable=no-member
+        self.birthyear.click()  # pylint:disable=no-member
+        self.gender.click()  # pylint:disable=no-member
+        self.sinup.click()  # pylint:disable=no-member
 
     def tearDown(self):
-        """this method will be called after every test"""
-        self.driver.quit()
+        """called after every test"""
+        TearDown(self.driver)
+
 
 if __name__ == "__main__":
-    """This is the main function will Run the Unit Test if this Moudle is not imported"""
-    unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(Test_03_Registration))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
