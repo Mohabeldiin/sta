25e84ed43c1de68a76d778cf603ae25791756f5e
Mohab Mohsen <mohabeldiin@gmail.com>
2022-06-28 00:32:58 +0200

    test 10

--- packages/testsuites/suite_registration/test_10_Registration.py
+++ packages/testsuites/suite_registration/test_10_Registration.py
@@ -2,17 +2,9 @@
     TC_10_Registration from https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
 
 
-import unittest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from packages.testsuites.suite_registration.init import TearDown, SetUp, TestData, unittest, project_logger, setup_selenium_driver
 
-
-class Test_Data(object):
-    """this class is enum holds the test data that is used in this test case"""
-    PHONE_NUMBER = "+9190112244"
+logger = project_logger("Registration Test Case 10")
 
 
 class Test_10_Registration(unittest.TestCase):
@@ -21,79 +13,23 @@ class Test_10_Registration(unittest.TestCase):
     2- Click on Register button."""
 
     def setUp(self):
-        """this method will be called before every test"""
-        self.driver = webdriver.Chrome("C:\\Program Files (x86)\\chromedriver.exe")
-        self.driver.implicitly_wait(5)
-        self.driver.get('https://www.facebook.com/')
-        self.driver.maximize_window()
-        self.login_form_locator = (By.CLASS_NAME,"_9vtf")
-        self.creat_new_account_locator = (By.LINK_TEXT, "Create New Account")
-
-        try:
-            if WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located(self.login_form_locator)
-            ).is_displayed():
-                self.creat_new_acount = WebDriverWait(self.driver, 10).until(
-                    EC.element_to_be_clickable(self.creat_new_account_locator)
-                )
-                self.creat_new_acount.click()
-        except TimeoutException:
-            print("\n###############\n",TimeoutException.__doc__ , "\n###############\n")
-            assert False
-
-        self.phone_textfiled_locator = (By.NAME,'reg_email__')
-        self.sinUp_button_locator = (By.NAME, "websubmit")
-        self.error_message_locator = (By.ID, "reg_error_inner")
+        """Called before every test"""
+        self.driver = setup_selenium_driver()
+        self.elements = SetUp(self.driver)
 
     def test_01_Phone_Number_validation(self):
         """Check the phone number when passing country code\n
         EC: It should not show any validation message"""
-        try:
-            try:
-                phone = WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located(self.phone_textfiled_locator)
-                )
-                phone.send_keys(Test_Data.PHONE_NUMBER)
-            except AssertionError:
-                print("\n###############\n",AssertionError.__doc__ , "\n###############\n")
-                assert False
-            except TimeoutException:
-                print("\n###############\n",TimeoutException.__doc__ , "Can't fined error message:", self.phone_textfiled_locator , "\n###############\n")
-                assert False
-            try:
-                sinup = WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located(self.sinUp_button_locator)
-                )
-                sinup.click()
-            except AssertionError:
-                print("\n###############\n",AssertionError.__doc__ , "\n###############\n")
-                assert False
-            except TimeoutException:
-                print("\n###############\n",TimeoutException.__doc__ , "Can't fined error message:", self.sinUp_button_locator , "\n###############\n")
-                assert False
-            try:
-                if WebDriverWait(self.driver, 10).until(
-                    EC.invisibility_of_element_located(self.error_message_locator)
-                ):
-                    assert True
-                else:
-                    print("EC != AC")
-                    assert False
-            except AssertionError:
-                print("\n###############\n",AssertionError.__doc__ , "\n###############\n")
-                assert False
-            except TimeoutException:
-                print("\n###############\n",TimeoutException.__doc__ , "Can't fined error message:", self.error_message_locator , "\n###############\n")
-                assert False
-        except AssertionError:
-            print("\n###############\n",AssertionError.__doc__ , "\n###############\n")
-            assert False
-
-    def tearDown(self):
-        """this method will be called after every test"""
-        self.driver.quit()
+        self.elements.email.send_keys(TestData.PHONE_NUMBER)
+        self.elements.sinup.click()
+        ER = True
+        AR = bool(self.elements.email.get_attribute('aria-invalid') == "true")
+        self.assertNotEqual(
+            AR, ER, "the phone number when not pass country code.")
 
 
 if __name__ == "__main__":
-    """This is the main function will Run the Unit Test if this Moudle is not imported"""
-    unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(Test_10_Registration))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
