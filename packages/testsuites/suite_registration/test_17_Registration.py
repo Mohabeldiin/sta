fa237f25896b0a312b0c46fdcc834e37f3a95e2d
Mohab Mohsen <mohabeldiin@gmail.com>
2022-06-28 12:45:34 +0200

    test 17

--- packages/testsuites/suite_registration/test_17_Registration.py
+++ packages/testsuites/suite_registration/test_17_Registration.py
@@ -1,41 +1,51 @@
-"""this test case foo
-    TC_foo_Registration from https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
+"""Phone Number Validation
+    TC_17_Registration from
+    https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
+
+from packages.testsuites.suite_registration.init import TearDown, SetUp, TestData, unittest, project_logger, setup_selenium_driver
 
 
-import unittest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+logger = project_logger("Registration Test Case 17")
 
 
-class Test_Data(object):
-    """this class is enum holds the test data that is used in this test case"""
-    pass
-
-
-class Test_foo_Registration(unittest.TestCase):
-    """foo\n
-    1- foo\n
-    2- foo"""
+class Test_17_Registration(unittest.TestCase):
+    """Verify if the length of the phone number is incorrect"""
 
     def setUp(self):
-        """this method will be called before every test"""
-        self.driver = webdriver.Chrome("C:\\Program Files (x86)\\chromedriver.exe")
-        self.driver.implicitly_wait(5)
-        self.driver.get('https://www.facebook.com/')
-        self.driver.maximize_window()
+        """called before every test"""
+        self.driver = setup_selenium_driver()
+        self.elements = SetUp(self.driver)
 
-    def test_foo_foo(self):
-        """foo\n
-        EC: foo"""
-        pass
+    def test_01_Registration(self):
+        """Verify if the length of the phone number is incorrect i.e. less than 10.
+            1- Enter phone number less than 10 digits.
+            2- Enter all required fields.
+            3- Click on Register Button.
+            EC: It should show the validation error message for phone number length."""
+
+        self.elements.fname.send_keys(TestData.FRIST_NAME)
+        self.elements.lname.send_keys(TestData.LAST_NAME)
+        self.elements.email.send_keys(
+            TestData.PHONE_NUMBER_LENGTH_LESS_THAN_MIN)
+        self.elements.password.send_keys(TestData.PASSWORD_NUM_LETTER)
+        self.elements.day.click()
+        self.elements.month.click()
+        self.elements.year.click()
+        self.elements.gender.click()
+        self.elements.sinup.click()
+        ER = True
+        AR = bool(self.elements.email.get_attribute(
+            'aria-invalid') == "true")
+        self.assertNotEqual(
+            AR, ER, "Length of the phone number is incorrect.")
 
     def tearDown(self):
-        """this method will be called after every test"""
-        self.driver.quit()
+        """called after every test"""
+        TearDown(self.driver)
 
 
 if __name__ == "__main__":
-    """This is the main function will Run the Unit Test if this Moudle is not imported"""
-    unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(Test_17_Registration))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
