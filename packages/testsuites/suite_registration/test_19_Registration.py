3c1f6509a2b30f8093f097bb86e2dbe736e6f200
Mohab Mohsen <mohabeldiin@gmail.com>
2022-06-28 13:28:10 +0200

    test 19 + 18

--- packages/testsuites/suite_registration/test_19_Registration.py
+++ packages/testsuites/suite_registration/test_19_Registration.py
@@ -1,41 +1,50 @@
-"""this test case foo
-    TC_foo_Registration from https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
+"""Password Validation
+    TC_19_Registration from
+    https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
+
+from packages.testsuites.suite_registration.init import TearDown, SetUp, TestData, unittest, project_logger, setup_selenium_driver
 
 
-import unittest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+logger = project_logger("Registration Test Case 19")
 
 
-class Test_Data(object):
-    """this class is enum holds the test data that is used in this test case"""
-    pass
-
-
-class Test_foo_Registration(unittest.TestCase):
-    """foo\n
-    1- foo\n
-    2- foo"""
+class Test_19_Registration(unittest.TestCase):
+    """Verify if the password required rules are not satisfied in the password"""
 
     def setUp(self):
-        """this method will be called before every test"""
-        self.driver = webdriver.Chrome("C:\\Program Files (x86)\\chromedriver.exe")
-        self.driver.implicitly_wait(5)
-        self.driver.get('https://www.facebook.com/')
-        self.driver.maximize_window()
+        """called before every test"""
+        self.driver = setup_selenium_driver()
+        self.elements = SetUp(self.driver)
 
-    def test_foo_foo(self):
-        """foo\n
-        EC: foo"""
-        pass
+    def test_01_registration(self):
+        """Verify if the password required rules are not satisfied in the password
+            1- Enter the password which not satisfies the required rule.
+            2- Click on Register button.
+            EC:It should display error with required rules for password value
+            (like it should contain a special character, a small case, a number)"""
+
+        self.elements.fname.send_keys(TestData.FRIST_NAME)
+        self.elements.lname.send_keys(TestData.LAST_NAME)
+        self.elements.email.send_keys(TestData.EMAIL_INVALID)
+        self.elements.password.send_keys(TestData.PASSWORD_LETTER)
+        self.elements.day.click()
+        self.elements.month.click()
+        self.elements.year.click()
+        self.elements.gender.click()
+        self.elements.sinup.click()
+        ER = True
+        AR = bool(self.elements.password.get_attribute(
+            'aria-invalid') == "true")
+        self.assertNotEqual(
+            AR, ER, "Length of the phone number is incorrect.")
 
     def tearDown(self):
-        """this method will be called after every test"""
-        self.driver.quit()
+        """called after every test"""
+        TearDown(self.driver)
 
 
 if __name__ == "__main__":
-    """This is the main function will Run the Unit Test if this Moudle is not imported"""
-    unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(Test_19_Registration))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
